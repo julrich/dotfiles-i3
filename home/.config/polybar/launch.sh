69397a0e1608766f5a8bc2db9a566107db535ffd
Jonas Ulrich | ruhmesmeile GmbH <jonas.ulrich@ruhmesmeile.com>
2020-04-23 19:37:27 +0200

    fix: multiple monitor support, design

--- home/.config/polybar/launch.sh
+++ home/.config/polybar/launch.sh
@@ -9,5 +9,14 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar
-polybar -c ~/.config/polybar/config.ini top &
-polybar -c ~/.config/polybar/config.ini bottom &
+# Support multiple monitors
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar -c ~/.config/polybar/config.ini top &
+    MONITOR=$m polybar -c ~/.config/polybar/config.ini bottom &
+  done
+else
+  polybar -c ~/.config/polybar/config.ini top &
+  polybar -c ~/.config/polybar/config.ini bottom &
+fi
+
